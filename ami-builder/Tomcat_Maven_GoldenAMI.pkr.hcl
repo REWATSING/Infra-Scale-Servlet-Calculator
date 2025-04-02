1bfee0c7b45371f709490f15484afa738e2badcd
Rewat <rewatsingh271@gmail.com>
2025-04-02 14:32:20 +0530

    Corrected apcker hcl file , added terraform code in packer folder to create iam profile to acces s3 bucket from base ami temprory insytance, roles are also temprary

--- ami-builder/Tomcat_Maven_GoldenAMI.pkr.hcl
+++ ami-builder/Tomcat_Maven_GoldenAMI.pkr.hcl
@@ -15,28 +15,9 @@ source "amazon-ebs" "EBS-is-source" {
   ssh_keypair_name       = "packerkey" # Name of your key pair in AWS
   ssh_private_key_file   = "~/packerkey.pem" # Path to your private key file
   ami_name               = "tomcat-mvn-golden-ami-{{timestamp}}"
-  iam_instance_profile   = "packer-instance-profile-{{timestamp}}"
-
-  # IAM Role & Instance Profile (Created Dynamically)
-  temporary_iam_instance_profile_policy_document = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        "Resource": [
-          "arn:aws:s3:::calculator-bucket-271271271271",
-          "arn:aws:s3:::calculator-bucket-271271271271/*"
-        ]
-      }
-    ]
-  }
-  EOF
+  iam_instance_profile   = "PackerInstanceProfile"   # Use IAM profile created by Terraform
 }
+
 
 build {
   sources = ["source.amazon-ebs.EBS-is-source"]
---
+++ ami-builder/terraform-packer-iam/iam.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# IAM Role for Packer
+resource "aws_iam_role" "packer" {
+  name = "PackerS3AccessRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# IAM Policy to Allow S3 Access
+resource "aws_iam_policy" "s3_read" {
+  name        = "PackerS3ReadPolicy"
+  description = "Allow Packer instances to read from S3"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = ["s3:GetObject", "s3:ListBucket"]
+      Resource = [
+        "arn:aws:s3:::calculator-bucket-271271271271",
+        "arn:aws:s3:::calculator-bucket-271271271271/*"
+      ]
+    }]
+  })
+}
+
+# Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "s3_attach" {
+  role       = aws_iam_role.packer.name
+  policy_arn = aws_iam_policy.s3_read.arn
+}
+
+# Create Instance Profile
+resource "aws_iam_instance_profile" "packer_profile" {
+  name = "PackerInstanceProfile"
+  role = aws_iam_role.packer.name
+}
